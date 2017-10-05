b142beb49a78d27a3b7b6721f9452eceb860757b
Arden Kim <kmk3170@uw.edu>
2017-10-05 12:49:58 -0700

    Initial Commit

---
+++ Hello-iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hello-iOS
+//
+//  Created by iGuest on 10/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
